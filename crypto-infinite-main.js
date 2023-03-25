32dce32465191943c59b3772e233f63cf317131e
inpodia <108050308+inpodia@users.noreply.github.com>
2023-03-25 23:26:07 +0800

    Update crypto-infinite-main.js

--- crypto-infinite-main.js
+++ crypto-infinite-main.js
@@ -6,7 +6,7 @@ rain.onload = function () {
   !function (_0x9b30x2) {
     if (config.infinite_scroll) {
       var _0x9b30x3 = 0;
-      var _0x9b30x4 = "<svg height='24' viewBox='0 0 24 24' width='24'><path d='M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z'/></svg>";
+      var _0x9b30x4 = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjFFJRvJfMczhbfivNqXBa96k7rkkX-FvdWZ-jTel8mvKC6y5nbfUXaRYS8satxX2Jfno3uLihMMFKFqyfFQQ2YDfG4f0i3cOzbyb-xJSPdIyWYgRbI3FXOXCc8No7CDJRe40xUIRu9mOcWUAZDmCcdA3Y65XNiz9GZRfysAtAOMAqOqO39KNQBIyFQ/s100/ipdloader.gif";
       var _0x9b30x5 = _0x9b30x2("#blog-pager"), _0x9b30x6 = _0x9b30x2(".blog-posts"), _0x9b30x7 = false;
       _0x9b30x5.find("#blog-pager-newer-link").remove(), _0x9b30x5.on("click", "#blog-pager-older-link a", function () {
         history.pushState("", "", this.href);
